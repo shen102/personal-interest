87b65c5bb67c684855cf533be783d5ff1f736f82
shen102 <sheenrodrigazo@gmail.com>
2023-10-17 15:24:23 +0800

    added

--- script.js
+++ script.js
@@ -5,6 +5,25 @@ const mobileMenu = () => {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 };
-
 menu.addEventListener('click', mobileMenu);
 
+
+
+
+const initSlider = () => {
+    const imageList = document.querySelector(".slider-wrapper .photo-list");
+    const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const direction = button.id === "prev-slide" ? -1 : 1;
+            const scrollAmount = imageList.clientWidth * direction;
+            imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+            });
+    });
+  
+}
+window.addEventListener("load", initSlider);
+
+ /* imageList.addEventListener("scroll", () => {
+        handSlideButtons
+    })*/
